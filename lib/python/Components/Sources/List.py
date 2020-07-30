08231eba82df2cec26ee4fbbf4bb832257e103d5
Hains <hainsvdbosch@ziggo.nl>
2020-07-30 20:44:48 +0200

    List.py: Do not use list as a variable
    Comment out assert statement for now.
    Filecommander is working now so far at least..
    
    Needs rework.

--- lib/python/Components/Sources/List.py
+++ lib/python/Components/Sources/List.py
@@ -11,9 +11,9 @@ setup the "fonts".
 
 This has been done so another converter could convert the list to a different format, for example
 to generate HTML."""
-	def __init__(self, list = [ ], enableWrapAround = False, item_height = 25, fonts = [ ]):
+	def __init__(self, list_ = [ ], enableWrapAround = False, item_height = 25, fonts = [ ]):
 		Source.__init__(self)
-		self.__list = list
+		self.__list = list_
 		self.onSelectionChanged = [ ]
 		self.item_height = item_height
 		self.fonts = fonts
@@ -21,8 +21,8 @@ to generate HTML."""
 		self.enableWrapAround = enableWrapAround
 		self.__style = "default" # style might be an optional string which can be used to define different visualisations in the skin
 
-	def setList(self, list):
-		self.__list = list
+	def setList(self, list_):
+		self.__list = list_
 		self.changed((self.CHANGED_ALL,))
 
 	list = property(lambda self: self.__list, setList)
@@ -99,12 +99,12 @@ to generate HTML."""
 
 	style = property(getStyle, setStyle)
 
-	def updateList(self, list):
+	def updateList(self, list_):
 		"""Changes the list without changing the selection or emitting changed Events"""
-		assert len(list) == len(self.__list)
+		# FIXME assert len(list_) == len(self.__list)
 		old_index = self.index
 		self.disable_callbacks = True
-		self.list = list
+		self.list = list_
 		self.index = old_index
 		self.disable_callbacks = False
 
